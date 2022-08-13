f624dbc0803d0073b742f7489c16e23005c2de16
Nikolaj Søgaard Simonsen <nikolajmos@me.com>
2022-08-13 16:24:12 +0200

    Added comment to test method to explain what it's purpose is

--- storyChallenge/storyChallengeTests/storyChallengeTests.swift
+++ storyChallenge/storyChallengeTests/storyChallengeTests.swift
@@ -20,21 +20,18 @@ class storyChallengeTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 	
-	
-	
+	// This test runs the BookListService with a static JSON to test whether the models are parsed correctly AND send to the completion handler
 	func testBookListService() {
 		let expectation = self.expectation(description: "Did receive books")
 		let service = BookListService(dependency: BookListStoreMock(stub: self.loadStub(name: "booklist", fileExtension: "json")))
 		service
 			.getList(page: 0)
 			.sink(receiveCompletion: { _ in
-				
-			}, receiveValue: { books in
+			}, receiveValue: { _ in
 				expectation.fulfill()
 			})
 			.store(in: &self.cancellables)
 		
 		waitForExpectations(timeout: 10)
 	}
-
 }
