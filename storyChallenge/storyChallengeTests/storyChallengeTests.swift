2b7d514927b3042d19d5681d553dd45ecc4af928
Nikolaj Søgaard Simonsen <nikolajmos@me.com>
2022-08-13 14:59:11 +0200

    Started implementing ViewModel Unit test

--- storyChallenge/storyChallengeTests/storyChallengeTests.swift
+++ storyChallenge/storyChallengeTests/storyChallengeTests.swift
@@ -5,10 +5,12 @@
 //  Created by Nikolaj Simonsen on 07/08/2022.
 //
 
+import Combine
 import XCTest
 @testable import storyChallenge
 
 class storyChallengeTests: XCTestCase {
+	private var cancellables: Set<AnyCancellable> = []
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,20 +19,25 @@ class storyChallengeTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+	
+	
+	func testBookListVM() throws {
+		let service = BookListService()
+		service
+			.getList(page: 0)
+			.sink { completion in
+				switch completion {
+				case .failure(let error):
+					break
+					
+				case .finished:
+					break
+				}
+			} receiveValue: { books in
+				
+			}
+			.store(in: &cancellables)
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+	}
 
 }
